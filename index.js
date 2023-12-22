ac40b9717f8327f4b33cbf37fa1a23641ad1075f
athiwat_tha@hotmail.com <athiwat_tha@hotmail.com>
2023-12-22 18:06:11 +0700

    edit

--- index.js
+++ index.js
@@ -1,100 +1,58 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
 const axios = require('axios');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-const messages = [];
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // ส่งข้อความมายัง client เมื่อเชื่อมต่อ
-  socket.emit('messages', messages);
-
-  // รับข้อมูลจาก client
-  socket.on('redeemablePoints', (data) => {
-    // นำข้อมูลไปใช้ต่อตรงนี้ตามที่คุณต้องการ
-    console.log(`Redeemable Points for "${data.msg}": ${data.redeemablePoints}`);
-  });
-
-  // จบการเชื่อมต่อ
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/messages', (req, res) => {
-  res.json(messages);
-});
+app.post('/get_redeemable_point', async (req, res) => {
+  try {
+    const walletNames = req.body.wallet_names.split('\n').filter(Boolean);
 
-app.post('/addMessage', async (req, res) => {
-  const { text } = req.body;
+    const redeemablePoints = await Promise.all(
+      walletNames.map(async (walletName) => {
+        const fetchConfig = {
+          json: true,
+          code: 'uspts.worlds',
+          scope: 'uspts.worlds',
+          table: 'userpoints',
+          lower_bound: walletName,
+          upper_bound: walletName,
+          index_position: 1,
+          key_type: '',
+          limit: 1,
+          reverse: false,
+          show_payer: false,
+        };
 
-  if (text) {
-    const messagesArray = text.split('\n');
-    const nonEmptyMessages = messagesArray.filter(msg => msg.trim() !== '');
+        const axiosConfig = {
+          method: 'POST',
+          url: 'https://wax.greymass.com/v1/chain/get_table_rows',
+          headers: {},
+          data: fetchConfig,
+        };
 
-    
-
-    // ใช้ Promise.all() เพื่อดึงข้อมูล redeemablePoints ทุกข้อความแบบ parallel
-    await Promise.all(nonEmptyMessages.map(async (msg) => {
-      // เพิ่มข้อความใหม่ลงใน array
-      messages.push(msg);
-
-      // ดึงค่า redeemable_points สำหรับแต่ละ message
-      const fetchConfig = {
-        json: true,
-        code: 'uspts.worlds',
-        scope: 'uspts.worlds',
-        table: 'userpoints',
-        lower_bound: msg,
-        upper_bound: msg,
-        index_position: 1,
-        key_type: '',
-        limit: 1,
-        reverse: false,
-        show_payer: false,
-      };
-
-      const axiosConfig = {
-        method: 'POST',
-        url: 'https://wax.greymass.com/v1/chain/get_table_rows',
-        headers: {},
-        data: fetchConfig,
-      };
-
-      try {
         const response = await axios(axiosConfig);
-        const redeemablePoints = parseFloat(response.data.rows[0].redeemable_points)/10;
+        const redeemablePoint = parseFloat(response.data.rows[0]?.redeemable_points)/10 || null;
+        //console.log(response.data.rows)
+        return { walletName, redeemablePoint };
+      })
+    );
 
-        console.log(`Redeemable Points for "${msg}": ${redeemablePoints}`);
-
-        // ส่งค่า redeemablePoints กลับไปยังหน้าเว็บ
-        io.emit('redeemablePoints', { msg, redeemablePoints });
-      } catch (error) {
-        console.error(`Error fetching redeemable points for "${msg}":`, error.message);
-      }
-    }));
-    // ล้าง array ข้อความเดิม
-    messages.length = 0;
-    res.send('เพิ่มข้อความเรียบร้อย');
-  } else {
-    res.status(400).send('กรุณาใส่ข้อความ');
+    res.json({ redeemablePoints });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
